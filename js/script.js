f424cb81a988f93f391c7c9c92fd262ca028ae95
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-10 13:40:02 -0500

    error corrected

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function init(){
   alert("Annesha Mondal:"+message);
-
+}
 
 var message= document.getElementById("entryinput").value;
 document.getElementById("textoutput").value=message;
@@ -8,6 +8,6 @@ document.getElementById("textoutput").value=message;
 var button= document.getElementById("entrybutton");
 button.addEventListener("click", init);
 
-}
+
 
 window.addEventListener('load', init);
