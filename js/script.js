f3b4ce04994f1932877de3fc36b0c7f44cbcd798
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-11 18:06:43 -0500

    colon added

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var button= document.getElementById("entrybutton");
 
 button.addEventListener("click", function()
 {
-  alert ("Annesha Mondal"+ message.value);
+  alert ("Annesha Mondal: "+ message.value);
   document.getElementById("textoutput").innerHTML=message.value
 });
 
