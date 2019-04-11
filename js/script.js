8db6827e0db5668f48a8371e05465d28da228966
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-11 17:30:28 -0500

    unnecessary code removed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function init(){
   alert("Annesha Mondal:"+message);
 }
 
-var message= document.getElementById("entryinput").value;
+var message= document.getElementById("entryinput");
 document.getElementById("textoutput").value=message;
 
 var button= document.getElementById('entrybutton');
