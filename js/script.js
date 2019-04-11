28d61b8ed8b8156945b1730b6e24a4350100c763
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-11 17:32:54 -0500

    lines of code moved to within the method

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 function init(){
-  alert("Annesha Mondal:"+message);
-}
+
+
 
 var message= document.getElementById("entryinput");
-
-
 var button= document.getElementById("entrybutton");
 
+button.addEventListener("click", function()
+{
+  alert ("Annesha Mondal"+ message.value);
+  document.getElementById("textoutput").innerHTML=message.value
+});
 
 
+}
 
 window.addEventListener('load', init);
