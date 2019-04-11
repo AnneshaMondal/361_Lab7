6a5b9895719a7058c578b60579c065fd94f00776
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-11 17:30:58 -0500

    double quotes added around entry button

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function init(){
 var message= document.getElementById("entryinput");
 document.getElementById("textoutput").value=message;
 
-var button= document.getElementById('entrybutton');
+var button= document.getElementById("entrybutton");
 button.addEventListener('click', init);
 
 
