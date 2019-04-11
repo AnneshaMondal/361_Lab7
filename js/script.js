36683c784a43bb5c748ae6714f90fac09686ecb3
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-11 18:11:37 -0500

    javascript formatted

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
-function init(){
+function init() {
 
 
+	var message = document.getElementById("entryinput");
+	var button = document.getElementById("entrybutton");
 
-var message= document.getElementById("entryinput");
-var button= document.getElementById("entrybutton");
-
-button.addEventListener("click", function()
-{
-  alert ("Annesha Mondal: "+ message.value);
-  document.getElementById("textoutput").innerHTML=message.value
-});
+	button.addEventListener("click", function () {
+		alert("Annesha Mondal: " + message.value);
+		document.getElementById("textoutput").innerHTML = message.value
+	});
 
 
 }
