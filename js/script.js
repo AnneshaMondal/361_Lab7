2916ac3a05a6e2e330a6ab6e1b2c625ce23024fe
Annesha Mondal <amondal@hawk.iit.edu>
2019-04-11 18:07:17 -0500

    correct js page copied from gh branch

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 function init(){
-  alert("Annesha Mondal:"+message);
+
+
+
+var message= document.getElementById("entryinput");
+var button= document.getElementById("entrybutton");
+
+button.addEventListener("click", function()
+{
+  alert ("Annesha Mondal: "+ message.value);
+  document.getElementById("textoutput").innerHTML=message.value
+});
+
+
 }
-
-var message= document.getElementById("entryinput").value;
-document.getElementById("textoutput").value=message;
-
-var button= document.getElementById('entrybutton');
-button.addEventListener('click', init);
-
-
 
 window.addEventListener('load', init);
